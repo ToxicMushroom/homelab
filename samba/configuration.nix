247ad0a90fa4ae1d7f69307a5bafffb1983ccf78
= <32853531+ToxicMushroom@users.noreply.github.com>
2026-01-11 18:49:57 +0100

    remove grub, pray it works now

--- samba/configuration.nix
+++ samba/configuration.nix
@@ -10,11 +10,6 @@
     ./disko-config.nix
   ];
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
   networking.hostName = "beverburcht"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
