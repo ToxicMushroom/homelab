ac83db672d65884068f36e7ad76379a8236b7950
= <32853531+ToxicMushroom@users.noreply.github.com>
2026-01-11 18:26:03 +0100

    I forgot import

--- samba/configuration.nix
+++ samba/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{  lib, pkgs, ... }:
+{  lib, pkgs, isImageTarget, ... }:
 
 {
   imports = lib.optionals (!isImageTarget) [
